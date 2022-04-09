cb1315d50937b85ee84fc1a9b6921a631c34a9ea
o-gi-s <o-gi-s@users.noreply.github.com>
2022-04-09 21:43:33 +0000

    deploy: 273f1a4239dd125fcf2c13c4fbd1daecd89a7699

---
+++ _next/static/F4oy_TP0wD5WHqlroNs61/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-91f2ae539237c79ce87c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4a9b731e5507d5aec89a.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
