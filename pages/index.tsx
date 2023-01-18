f7fd535bb345130bf0f8fa5305bb8f890cd85035
Mochan01 <ryohei.s@fantamstick.jp>
2023-01-18 20:36:19 +0900

    Append new option 'only local'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import Head from "next/head";
-import React, { useState } from 'react';
+import React, { useRef, useState } from "react";
 import { Box, Button } from "@material-ui/core";
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
+import Checkbox from "@material-ui/core/Checkbox";
 import { TextFieldTd } from "../components/atoms/TextFieldTd";
 import packageJson from "../package.json";
 
 export default function Home() {
-
   const [state, setState] = useState({
     inputVal: "",
-    resultVal: ""
+    resultVal: "",
   });
+
+  // 管理 ローカル消去のみ
+  const defaultChecked = false;
+  const isOnlyLocal = useRef(defaultChecked);
 
   const createCommand = (val: string): string => {
     const lines = val.split("\n");
-    const commands = lines.map(version => {
-      return `git tag -d ${ version }; git push origin :${ version };`
+    const commands = lines.map((version) => {
+      let command = `git tag -d ${version};`;
+      if (!isOnlyLocal.current) command += ` git push origin :${version};`;
+      return command;
     });
     return commands.join(" ");
-  }
+  };
 
   return (
     <>
@@ -27,51 +33,64 @@ export default function Home() {
       </Head>
       <Box textAlign="center">
         <Box mb="200px" />
-        <Typography variant="h5">
-          TD command generator
-        </Typography>
-        <Typography variant="subtitle1">{ packageJson.version }</Typography>
+        <Typography variant="h5">TD command generator</Typography>
+        <Typography variant="subtitle1">{packageJson.version}</Typography>
         <Box mb="30px" />
         <Typography variant="caption">
-          gitでタグを一気に消したいときに。<br />
-          ローカルで消去したあとにリモートも消すコマンドを生成します。
+          gitでタグを一気に消したいときに。
+          <br />
+          gitのtagを消去するコマンドを生成します。
         </Typography>
+        <Box mb="30px" />
+        <Box component="label">
+          <Checkbox
+            { ...{ defaultChecked } }
+            onChange={(e) => {
+              isOnlyLocal.current = e.currentTarget.checked;
+            }}
+          />
+          ローカル消去のみ
+        </Box>
         <Box mb="30px" />
         <Box display="flex" justifyContent="center">
           <Box>
             <TextFieldTd
-              placeholder={ <>v0.0.1<br />v0.0.2</> }
-              onChange={ (value) => {
+              placeholder={
+                <>
+                  v0.0.1
+                  <br />
+                  v0.0.2
+                </>
+              }
+              onChange={(value) => {
                 setState({ ...state, inputVal: value });
-              } }
+              }}
             />
             <Box mb="30px" />
             <Button
               variant="contained"
               color="primary"
-              onClick={ () => {
+              onClick={() => {
                 const isEmpty = !state.inputVal || !state.inputVal.match(/\S/g);
                 if (isEmpty) return;
                 const resultVal = createCommand(state.inputVal);
                 setState({ ...state, resultVal });
-              } }
+              }}
             >
               Export
             </Button>
           </Box>
           <Box width="30px" />
           <Box>
-            <TextFieldTd
-              defaultValue={ state.resultVal }
-            />
+            <TextFieldTd defaultValue={state.resultVal} />
             <Box mb="30px" />
             <Button
               variant="contained"
               color="secondary"
-              onClick={ () => {
-                if(!navigator.clipboard) return;
+              onClick={() => {
+                if (!navigator.clipboard) return;
                 navigator.clipboard.writeText(state.resultVal);
-              } }
+              }}
             >
               結果をコピー
             </Button>
@@ -79,5 +98,5 @@ export default function Home() {
         </Box>
       </Box>
     </>
-  )
+  );
 }
